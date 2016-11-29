ae933d6f31ce9c3dbe0ed35a133c058df7ab93ca
Wang Zi Xiao <328249923@qq.com>
2016-11-29 16:58:27 +0800

    react-native-screenshot-observer

---
+++ ios/RNScreenshotNotification/RNScreenshotNotification.h
@@ -0,0 +1,16 @@
+//
+//  RNScreenshotNotification.h
+//  RNScreenshotNotification
+//
+//  Created by wangzixiao on 16/11/24.
+//  Copyright © 2016年 WangZiXiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
+
+@interface RNScreenshotNotification : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNScreenshotNotification/RNScreenshotNotification.m
@@ -0,0 +1,110 @@
+//
+//  RNScreenshotNotification.m
+//  RNScreenshotNotification
+//
+//  Created by wangzixiao on 16/11/24.
+//  Copyright © 2016年 WangZiXiao. All rights reserved.
+//
+
+#import "RNScreenshotNotification.h"
+#import "RCTEventDispatcher.h"
+
+@interface RNScreenshotNotification ()
+
+@end
+
+@implementation RNScreenshotNotification
+
+@synthesize bridge = _bridge;
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(addObserverScreenshot) {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userDidTakeScreenshot:) name:UIApplicationUserDidTakeScreenshotNotification object:nil];
+    
+}
+
+RCT_EXPORT_METHOD(removeObserver:(NSString *)notificationName)
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIApplicationUserDidTakeScreenshotNotification
+                                                  object:nil];
+}
+
+- (void)userDidTakeScreenshot: (NSNotification *)notification {
+    NSLog(@"检测到截屏");
+    
+    //人为截屏, 模拟用户截屏行为, 获取所截图片
+    UIImage *image_ = [self imageWithScreenshot];
+    
+    //把图片存沙盒
+    NSString *path_sandox = NSHomeDirectory();
+    //设置一个图片的存储路径
+    NSString *imagePath = [path_sandox stringByAppendingString:@"/Documents/Screenshot.png"];
+    //把图片直接保存到指定的路径
+    [UIImagePNGRepresentation(image_) writeToFile:imagePath atomically:YES];
+    
+    [self.bridge.eventDispatcher sendDeviceEventWithName:@"ScreenshotObserver"
+                                                    body:@{ @"imagePath": imagePath ? imagePath : [NSNull null] }];
+}
+
+// 返回截取到的图片
+- (UIImage *)imageWithScreenshot
+{
+    NSData *imageData = [self dataWithScreenshotInPNGFormat];
+    return [UIImage imageWithData:imageData];
+}
+
+// 截取当前屏幕
+- (NSData *)dataWithScreenshotInPNGFormat
+{
+    CGSize imageSize = CGSizeZero;
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (UIInterfaceOrientationIsPortrait(orientation))
+        imageSize = [UIScreen mainScreen].bounds.size;
+    else
+        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+    
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    for (UIWindow *window in [[UIApplication sharedApplication] windows])
+    {
+        CGContextSaveGState(context);
+        CGContextTranslateCTM(context, window.center.x, window.center.y);
+        CGContextConcatCTM(context, window.transform);
+        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+        if (orientation == UIInterfaceOrientationLandscapeLeft)
+        {
+            CGContextRotateCTM(context, M_PI_2);
+            CGContextTranslateCTM(context, 0, -imageSize.width);
+        }
+        else if (orientation == UIInterfaceOrientationLandscapeRight)
+        {
+            CGContextRotateCTM(context, -M_PI_2);
+            CGContextTranslateCTM(context, -imageSize.height, 0);
+        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+            CGContextRotateCTM(context, M_PI);
+            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+        }
+        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
+        {
+            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+        }
+        else
+        {
+            [window.layer renderInContext:context];
+        }
+        CGContextRestoreGState(context);
+    }
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return UIImagePNGRepresentation(image);
+}
+
+-(void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+@end
